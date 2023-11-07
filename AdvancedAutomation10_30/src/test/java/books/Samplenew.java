ce93cea98ae341083c8b8547ae5ce1556470d3a2
gopiv <gopiv@Thanku>
2023-11-07 13:59:45 +0530

    This is commit from TE

--- AdvancedAutomation10_30/src/test/java/books/Samplenew.java
+++ AdvancedAutomation10_30/src/test/java/books/Samplenew.java
@@ -4,7 +4,7 @@ public class Samplenew {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("done");
+       System.out.println("hello");
 	}
 
 }
